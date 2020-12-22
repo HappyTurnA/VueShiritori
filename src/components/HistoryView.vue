da4466819fb70abc22de6214aa70b553f89c51a0
Kent-t0408 <75287221+Kent-t0408@users.noreply.github.com>
2020-12-22 13:15:39 +0900

    カタカナ入力対応しました。

--- src/components/HistoryView.vue
+++ src/components/HistoryView.vue
@@ -31,8 +31,36 @@ export default {
       const lastWord = this.m_historys[this.m_historys.length - 1];
       const lastChar = lastWord[lastWord.length - 1];
       const startChar = word[0];
-      return lastChar === startChar;
+
+      // 変換代入先
+      let convLastChar = "";
+      let convStartChar = "";
+
+      // 文字を平仮名に変換
+      convLastChar = convHira(lastChar);
+      convStartChar = convHira(startChar);
+
+      return convLastChar === convStartChar;
     },
   },
 };
+
+// 平仮名変換関数
+// pram : 1文字
+// ret  : 平仮名に変換された一文字、paramが平仮名の場合はfalseを返す
+function convHira(str) {
+
+  const Kata_Start = 12449;   // カタカナの最初の文字コード
+  const Kata_End   = 12538;   // カタカナの最後の文字コード
+
+  // カタカナなら平仮名に変換
+  if (Kata_Start <= str.charCodeAt() <= Kata_End) {
+    return str.replace(/[\u30A1-\u30FA]/g, (ch) =>
+      String.fromCharCode(ch.charCodeAt(0) - 0x60)
+    );
+  }else{
+    return false;
+  }
+}
+
 </script>
